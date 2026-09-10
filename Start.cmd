d1cac34315747137eb0e149ecd02919200d63207
x0XP <3501503+x0XP@users.noreply.github.com>
2026-09-10 12:55:01 +0100

    Compile shell layout fix

--- Start.cmd
+++ Start.cmd
@@ -7,7 +7,7 @@ if not exist "%CSC%" (
  echo Microsoft .NET Framework 4.x is required. Enable it in Windows Features.
  exit /b 1
 )
-"%CSC%" /nologo /target:winexe /optimize+ /out:"VideoShelf.exe" /reference:System.dll /reference:System.Core.dll /reference:System.Drawing.dll /reference:System.Windows.Forms.dll /reference:System.Net.Http.dll /reference:System.Web.Extensions.dll /reference:System.Xml.Linq.dll /reference:System.Security.dll "AppDataPaths.cs" "XdolfTheme.cs" "MockupControls.cs" "FolderManagement.cs" "TransferBridge.cs" "VideoShelf.cs" "ScreenshotHarness.cs" "Shelf.Core.cs" "Shelf.Library.cs" "Shelf.Online.cs" "PortraitLookup.cs" "OnlineSearch.cs" "OnlineThumbnailLookup.cs"
+"%CSC%" /nologo /target:winexe /optimize+ /out:"VideoShelf.exe" /reference:System.dll /reference:System.Core.dll /reference:System.Drawing.dll /reference:System.Windows.Forms.dll /reference:System.Net.Http.dll /reference:System.Web.Extensions.dll /reference:System.Xml.Linq.dll /reference:System.Security.dll "AppDataPaths.cs" "XdolfTheme.cs" "MockupControls.cs" "FolderManagement.cs" "TransferBridge.cs" "VideoShelf.cs" "ScreenshotHarness.cs" "Shelf.Core.cs" "Shelf.Layout.cs" "Shelf.Library.cs" "Shelf.Online.cs" "PortraitLookup.cs" "OnlineSearch.cs" "OnlineThumbnailLookup.cs"
 if errorlevel 1 (
  echo Build failed.
  exit /b 1
