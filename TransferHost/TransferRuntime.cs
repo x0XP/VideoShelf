904c0845769c148f312c73670f5cc6bb0713530f
x0XP <3501503+x0XP@users.noreply.github.com>
2026-09-11 19:15:54 +0100

    Cover transfer source resolution in runtime self-test

--- TransferHost/TransferRuntime.cs
+++ TransferHost/TransferRuntime.cs
@@ -8,6 +8,7 @@ internal static class TransferRuntime
     public static void SelfTest()
     {
         TorrentSession.SelfTest();
+        TransferSourceResolver.SelfTest();
         MethodInfo? metadataStart = typeof(TorrentManager)
             .GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
             .FirstOrDefault(m => m.Name == "StartAsync" &&
