71ebf0832bc3042a6b326a0e6352282e22a5884e
x0XP <3501503+x0XP@users.noreply.github.com>
2026-09-11 20:10:20 +0100

    Validate fullscreen player control in screenshots

--- TransferHost/TransferUiCapture.cs
+++ TransferHost/TransferUiCapture.cs
@@ -8,7 +8,11 @@ internal static class TransferUiCapture
     {
         Directory.CreateDirectory(outputDirectory);
         CaptureForm(new DownloadForm("magnet:?xt=urn:btih:0123456789012345678901234567890123456789", Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "VideoShelf Downloads"), "Re:Zero – Starting Life in Another World (S1) [1080p]", true), Path.Combine(outputDirectory, "VideoShelf-transfer-download.png"));
-        CaptureForm(new StreamForm("magnet:?xt=urn:btih:0123456789012345678901234567890123456789", "Re:Zero – Starting Life in Another World (S1) [1080p]", true), Path.Combine(outputDirectory, "VideoShelf-transfer-player.png"));
+
+        var stream = new StreamForm("magnet:?xt=urn:btih:0123456789012345678901234567890123456789", "Re:Zero – Starting Life in Another World (S1) [1080p]", true);
+        using (FullscreenPlayerController.Attach(stream))
+            CaptureForm(stream, Path.Combine(outputDirectory, "VideoShelf-transfer-player.png"));
+
         CaptureForm(new FileListForm("magnet:?xt=urn:btih:0123456789012345678901234567890123456789", "Re:Zero – Starting Life in Another World (S1) [1080p]", true), Path.Combine(outputDirectory, "VideoShelf-transfer-files.png"));
     }
 
