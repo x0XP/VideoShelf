e07d62bf395075c2fd289638ec5e85871548c5ea
x0XP <3501503+x0XP@users.noreply.github.com>
2026-09-12 08:07:11 +0100

    Cover stream selector in runtime self-test

--- TransferHost/TransferRuntime.cs
+++ TransferHost/TransferRuntime.cs
@@ -11,6 +11,7 @@ internal static class TransferRuntime
         StreamingTorrentSession.SelfTest();
         TransferSourceResolver.SelfTest();
         TorrentVideoSelection.SelfTest();
+        TorrentVideoSelectionUiController.SelfTest();
         MethodInfo? metadataStart = typeof(TorrentManager)
             .GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
             .FirstOrDefault(m => m.Name == "StartAsync" &&
